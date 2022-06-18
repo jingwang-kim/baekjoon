78afb674c6b6044951e99016a9d5460b6e0e4101
Jingwang Kim <wlsrhkd112@naver.com>
2022-06-18 17:12:49 +0900

    Add: 1406 stack

---
+++ 1406_python_success.py
@@ -0,0 +1,20 @@
+import sys
+
+str1 = list(sys.stdin.readline().rstrip())
+str2 = []
+
+for _ in range(int(sys.stdin.readline())):
+    command = list(sys.stdin.readline().split())
+    if command[0] == 'L':
+        if str1:
+            str2.append(str1.pop())
+    elif command[0] == 'D':
+        if str2:
+            str1.append(str2.pop())
+    elif command[0] == 'B':
+        str1.pop()
+    else:
+        str1.append(command[1])
+
+str1.extend(reversed(str2))
+print("".join(str1))
