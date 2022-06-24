ac749932b2c17a3e599c23e583a25a57c9768bd4
Jingwang Kim <wlsrhkd112@naver.com>
2022-06-24 15:48:00 +0900

    Add: 15651 backtracking

---
+++ 15651_python.py
@@ -0,0 +1,14 @@
+import sys
+
+n,m = map(int, sys.stdin.readline().split())
+answer = []
+
+def back():
+    if len(answer) == m:
+        print(' '.join(map(str,answer)))
+        return
+    for i in range(1,n+1):
+        answer.append(i)
+        back()
+        answer.pop()
+back()
