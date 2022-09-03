1d49ea17d0ffb54ebc1e9390e6e43f8916f6ddb8
Jingwang Kim <wlsrhkd112@naver.com>
2022-09-03 13:22:52 +0900

    Add: 10845 queue

---
+++ 10845_python.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+queue = deque([])
+for _ in range(n):
+    str = list(sys.stdin.readline().split())
+    if str[0] == 'push':
+        queue.append(str[1])
+    elif str[0] == 'pop':
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif str[0] == 'size':
+        print(len(queue))
+    elif str[0] == 'empty':
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif str[0] == 'front':
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    elif str[0] == 'back':
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
