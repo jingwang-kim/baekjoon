73497f49f5c02644a6e5728917022bb40470e2e9
Jingwang Kim <wlsrhkd112@naver.com>
2022-11-05 10:15:43 +0900

    PriorityQueue

---
+++ 11279_python.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
+hq = []
+
+for i in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if hq:
+            print(heapq.heappop(hq)*-1)
+        else:
+            print(0)
+    
+    else:
+        heapq.heappush(hq,-x)
