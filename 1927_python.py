cfc7b67ebca1385510d0ba3ac44e38b5444b9f91
Jingwang Kim <wlsrhkd112@naver.com>
2022-11-05 10:29:21 +0900

    PriorityQueue

---
+++ 1927_python.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
+hq = []
+
+for i in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if hq:
+            print(heapq.heappop(hq))
+        else:
+            print(0)
+    
+    else:
+        heapq.heappush(hq,x)
+        
