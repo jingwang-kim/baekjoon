0c66d1f0f7492feb9daf09050c1010bfde9cabb5
Jingwang Kim <wlsrhkd112@naver.com>
2022-12-13 17:49:52 +0900

    dp

---
+++ 11052_python.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline())
+p = [0]+list(map(int,sys.stdin.readline().split()))
+dp = [0] * (n+1)
+dp[1] = p[1]
+
+for i in range(2,n+1):
+    for j in range(1,i+1):
+        if dp[i] < dp[i-j] + p[j]:
+            dp[i] = dp[i-j] + p[j]
+print(dp[n])
